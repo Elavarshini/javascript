73d242a08fcf4547fa62992dfa45b1e8b5df696f
Elavarshini <elavarshini2002@gmail.com>
2024-07-09 19:31:55 +0530

    updated

--- 02_JS_variables/Ch2_pgm_08.js
+++ 02_JS_variables/Ch2_pgm_08.js
@@ -9,6 +9,10 @@ score = score + 50;
 console.log("New score: " + score);
 
 console.log("Way to go!");
+console.log("The player splats a kumquat.");
+score=score+100;
+console.log("new score: " + score);
+console.log("congratulations!!!");
 
 
 
--- 02_JS_variables/Ch2_pgm_09.js
+++ 02_JS_variables/Ch2_pgm_09.js
@@ -5,11 +5,6 @@ var $noProblemHere;
 var _underscore56;
 var StartWithCapital;
 var z5;
-
-var 999;
-var 39Steps;
-var &nope;
-var single words only;
 var yield;
 
 
